3ad4759f2de224e0c6eabd439bacd837b200bf3e
Sandeep Yadav <63005190+sandeepyadav12@users.noreply.github.com>
2022-05-09 12:50:59 +0545

    basic changes

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home - Click & Collect Groceries</title>
 </head>
 
 <body>
-    <div "><?php include './nav3.php'; ?></div>
+    <div><?php include './nav3.php'; ?></div>
     <div> <?php include './Dealofday.php'; ?></div>
     <div> <?php include './Topcategories.php'; ?></div>
 
