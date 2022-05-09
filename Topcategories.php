7474265f31626673c7b45ba8db12b36e1ca5213d
janakjk21 <janakjk21@gmail.com>
2022-05-09 15:46:33 +0545

    changes to animations and home pages

--- Topcategories.php
+++ Topcategories.php
@@ -15,25 +15,25 @@
 
 <body>
 	<div class="section-element vc_custom_1625904825611 padding-top-40 padding-bottom-40 margin-top-50 mobile-margin-top-30">
-		<div class="row">
+		<div class="row" style="margin-left:auto;margin-right:auto">
 			<div class="large-12 nasa-col columns">
 				<div class="nasa-dft nasa-title clearfix nasa-l hr-type-none text-center margin-bottom-10">
 					<h3 class="nasa-heading-title">Top Categories</h3>
 				</div>
-				<div class="category-slider nasa-category-slider-horizontal">
-					<div class="nasa-slider-items-margin nasa-slick-slider nasa-slick-nav nasa-slick-nav-outside nasa-category-horizontal-4 slick-initialized slick-slider" data-columns="6" data-columns-small="2" data-columns-tablet="3" data-autoplay="false" data-slides-all="false" data-switch-tablet="767" data-switch-desktop="1024">
-						<a class="nasa-nav-arrow slick-prev slick-arrow slick-disabled" href="javascript:void(0);" aria-disabled="true" style=""></a>
+				<div class="category-slider">
+					<div class="nasa-slider-items-margin nasa-slick-slider nasa-slick-nav nasa-slick-nav-outside nasa-category-horizontal-6 slick-initialized slick-slider" data-columns="7" data-columns-small="2" data-columns-tablet="3" data-autoplay="false" data-slides-all="false" data-switch-tablet="767" data-switch-desktop="1024">
+						<a class="nasa-nav-arrow " aria-disabled="true" style=""></a>
 
 						<div class="slick-list draggable">
 							<div class="slick-track" style="
 										opacity: 1;
-										width: 2261px;
-										transform: translate3d(0px, 0px, 0px);
+										width: 2300px;
+										
 									">
 								<div class="product-category nasa-slider-item wow fadeInUp slick-slide slick-current slick-active" data-wow-duration="1s" data-wow-delay="10ms" style="width: 323px" tabindex="0" data-slick-index="0" aria-hidden="false">
 									<a class="nasa-cat-link" href="https://elessi.nasatheme.com/product-category/organics/drinks/milks-creams/" title="Milks &amp; Creams" tabindex="0">
 										<div class="nasa-cat-thumb text-center nasa-transition">
-											<img src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat5.jpg" alt="Milks &amp; Creams" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat5.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat5.jpg" alt="Milks &amp; Creams" width="277" height="196" /></noscript>
+											<img src="./Assets/product04.webp" alt="Milks &amp; Creams" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat5.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat5.jpg" alt="Milks &amp; Creams" width="277" height="196" /></noscript>
 										</div>
 										<h3 class="header-title text-center nasa-cat-title">
 											Milks &amp; Creams
@@ -43,7 +43,7 @@
 								<div class="product-category nasa-slider-item wow fadeInUp slick-slide slick-active" data-wow-duration="1s" data-wow-delay="20ms" style="width: 323px" tabindex="0" data-slick-index="1" aria-hidden="false">
 									<a class="nasa-cat-link" href="https://elessi.nasatheme.com/product-category/organics/fruits/" title="Fruits" tabindex="0">
 										<div class="nasa-cat-thumb text-center nasa-transition">
-											<img src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat7.jpg" alt="Fruits" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat7.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat7.jpg" alt="Fruits" width="277" height="196" /></noscript>
+											<img src="./Assets/product05.webp" alt="Fruits" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat7.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat7.jpg" alt="Fruits" width="277" height="196" /></noscript>
 										</div>
 										<h3 class="header-title text-center nasa-cat-title">
 											Fruits
@@ -53,53 +53,26 @@
 								<div class="product-category nasa-slider-item wow fadeInUp slick-slide slick-active" data-wow-duration="1s" data-wow-delay="30ms" style="width: 323px" tabindex="0" data-slick-index="2" aria-hidden="false">
 									<a class="nasa-cat-link" href="https://elessi.nasatheme.com/product-category/organics/vegetables/" title="Vegetables" tabindex="0">
 										<div class="nasa-cat-thumb text-center nasa-transition">
-											<img src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat6.jpg" alt="Vegetables" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat6.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat6.jpg" alt="Vegetables" width="277" height="196" /></noscript>
+											<img src="./Assets//product06.webp" alt="Vegetables" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat6.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat6.jpg" alt="Vegetables" width="277" height="196" /></noscript>
 										</div>
 										<h3 class="header-title text-center nasa-cat-title">
 											Vegetables
 										</h3>
 									</a>
 								</div>
-								<div class="product-category nasa-slider-item wow fadeInUp slick-slide" data-wow-duration="1s" data-wow-delay="40ms" style="width: 323px" tabindex="-1" data-slick-index="3" aria-hidden="true">
+								<div class="product-category nasa-slider-item wow fadeInUp slick-slide" data-wow-duration="1s" data-wow-delay="40ms" style="width: 400px" tabindex="-1" data-slick-index="3" aria-hidden="true">
 									<a class="nasa-cat-link" href="https://elessi.nasatheme.com/product-category/organics/fruits/nut-gifts/" title="Nut Gifts" tabindex="-1">
 										<div class="nasa-cat-thumb text-center nasa-transition">
-											<img src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat1.jpg" alt="Nut Gifts" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat1.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat1.jpg" alt="Nut Gifts" width="277" height="196" /></noscript>
+											<img src="./Assets/product07.webp" alt="Nut Gifts" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat1.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat1.jpg" alt="Nut Gifts" width="277" height="196" /></noscript>
 										</div>
 										<h3 class="header-title text-center nasa-cat-title">
 											Nut Gifts
 										</h3>
 									</a>
 								</div>
-								<div class="product-category nasa-slider-item wow fadeInUp slick-slide" data-wow-duration="1s" data-wow-delay="50ms" style="width: 323px" tabindex="-1" data-slick-index="4" aria-hidden="true">
-									<a class="nasa-cat-link" href="https://elessi.nasatheme.com/product-category/organics/fruits/nut-gifts/butter-egges/" title="Butter &amp; Egges" tabindex="-1">
-										<div class="nasa-cat-thumb text-center nasa-transition">
-											<img src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat4.jpg" alt="Butter &amp; Egges" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat4.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat4.jpg" alt="Butter &amp; Egges" width="277" height="196" /></noscript>
-										</div>
-										<h3 class="header-title text-center nasa-cat-title">
-											Butter &amp; Egges
-										</h3>
-									</a>
-								</div>
-								<div class="product-category nasa-slider-item wow fadeInUp slick-slide" data-wow-duration="1s" data-wow-delay="60ms" style="width: 323px" tabindex="-1" data-slick-index="5" aria-hidden="true">
-									<a class="nasa-cat-link" href="https://elessi.nasatheme.com/product-category/organics/fresh-meats/" title="Fresh Meats" tabindex="-1">
-										<div class="nasa-cat-thumb text-center nasa-transition">
-											<img src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat3.jpg" alt="Fresh Meats" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat3.jpg" data-ll-status="loaded" class="entered lazyloaded" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat3.jpg" alt="Fresh Meats" width="277" height="196" /></noscript>
-										</div>
-										<h3 class="header-title text-center nasa-cat-title">
-											Fresh Meats
-										</h3>
-									</a>
-								</div>
-								<div class="product-category nasa-slider-item wow fadeInUp slick-slide" data-wow-duration="1s" data-wow-delay="70ms" style="width: 323px" tabindex="-1" data-slick-index="6" aria-hidden="true">
-									<a class="nasa-cat-link" href="https://elessi.nasatheme.com/product-category/organics/trees/" title="Trees" tabindex="-1">
-										<div class="nasa-cat-thumb text-center nasa-transition">
-											<img src="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20277%20196'%3E%3C/svg%3E" alt="Trees" width="277" height="196" data-lazy-src="https://f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat2.jpg" /><noscript><img src="../../f5d5a4q7.rocketcdn.me/wp-content/uploads/2016/06/organic-cat2.jpg" alt="Trees" width="277" height="196" /></noscript>
-										</div>
-										<h3 class="header-title text-center nasa-cat-title">
-											Trees
-										</h3>
-									</a>
-								</div>
+
+
+
 							</div>
 						</div>
 						<a class="nasa-nav-arrow slick-next slick-arrow" href="javascript:void(0);" style="" aria-disabled="false"></a>
